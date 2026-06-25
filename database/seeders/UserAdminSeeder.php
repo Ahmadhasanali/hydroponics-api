59859c46c23a923e353d1ff929e4123b9f257615
Hasan <ahmadhasanali24@gmail.com>
2026-06-26 06:57:02 +0700

    feat: add admin user seeder

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserAdminSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'superadmin',
+            'password' => Hash::make('password123#@!'),
+            'is_admin' => true,
+        ]);
+    }
+}
