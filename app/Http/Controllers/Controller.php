a274a991549cacfa5774ddee438e9a881d82522d
Hasan <ahmadhasanali24@gmail.com>
2026-08-03 08:48:57 +0700

    feat: helper hasFarm & selectedFarm di base Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Farm;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 abstract class Controller
 {
     use AuthorizesRequests;
+
+    protected function hasFarm(Request $request): bool
+    {
+        return $request->user()->farms()->exists();
+    }
+
+    protected function selectedFarm(Request $request): ?Farm
+    {
+        $farmId = $request->session()->get('selected_farm_id');
+
+        if ($farmId) {
+            $farm = $request->user()->farms()->find($farmId);
+
+            if ($farm) {
+                return $farm;
+            }
+        }
+
+        return $request->user()->farms()->first();
+    }
 }
